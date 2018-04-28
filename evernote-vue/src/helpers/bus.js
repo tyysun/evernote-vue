8806d07705f01f720bf9c9d2657b8a1e7d669b93
tyysun <tangyysun@gmail.com>
2018-04-28 22:39:25 +0800

    修改登录组件

---
+++ evernote-vue/src/helpers/bus.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+export default new Vue()
+
+
+// import  Bus from '@/helpers/bus'
+
+// Bus.$on('test',msg => {
+//   console.log(msg)
+// })
+
+
+// Bus.$emit('test','hellow jirengu')
